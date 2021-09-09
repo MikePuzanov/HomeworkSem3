a8c4eb9d6c4f6699ea13c3b2a1fb5e28d8171261
MikePuzanov <mikepuzanov@mail.ru>
2021-09-09 22:18:28 +0300

    add FileFunctions.cs and MatrixFunctions.cs

---
+++ ParallelMatrixMultiplication/ParallelMatrixMultiplication/FileFunctions.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.IO;
+
+namespace ParallelMatrixMultiplication
+{
+    /// <summary>
+    /// File's function
+    /// </summary>
+    public static class FileFunctions
+    {
+        private static (int, int) CountSizeMatrix(string filePath)
+        {
+            using var file = new StreamReader(filePath);
+            string line = file.ReadLine();
+            int size = 0;
+            if (line == null)
+            {
+                throw new Exception();
+            }
+            string[] lineDrop = line.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            while (line != null)
+            {
+                size++;
+                line = file.ReadLine();
+            }
+            return (size, lineDrop.Length);
+        }
+
+        /// <summary>
+        /// Create Marrix from file
+        /// </summary>
+        public static int[,] CreateMatrix(string filePath)
+        {
+            if (filePath == "")
+            {
+                throw new Exception();
+            }
+
+            (int lenght, int width) size = CountSizeMatrix((filePath));
+            var matrix = new int[size.lenght, size.width];
+            using var file = new StreamReader(filePath);
+            string line = file.ReadLine();
+            while (line != null)
+            {
+                var index = 0;
+                string[] lineDrop = line.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                for (int i = 0; i < size.lenght; ++i)
+                {
+                    matrix[index, i] = Int32.Parse(lineDrop[i]);
+                }
+                line = file.ReadLine();
+            }
+
+            return matrix;
+        }
+
+        /// <summary>
+        /// Create file and return matrix
+        /// </summary>
+        public static void CreateFileWithMatrix(string filePath, int[,] matrix)
+        {
+            if (filePath == "")
+            {
+                throw new Exception();
+            }
+            var fileOut = new FileInfo(filePath);
+            if (fileOut.Exists)
+            {
+                fileOut.Delete();
+            }
+            using var newFile = new FileStream(filePath, FileMode.Create);
+            var file = new StreamWriter(newFile);
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                var line = "";
+                for (int j = 0; j < matrix.Length; j++)
+                {
+                    line = $"{matrix[i, j]} ";
+                }
+                file.WriteLine(line);
+            }
+            file.Close();
+            if (fileOut.Exists)
+            {
+                fileOut.MoveTo(filePath);
+            }
+        }
+    }
+}
---
+++ ParallelMatrixMultiplication/ParallelMatrixMultiplication/MatrixFunctions.cs
@@ -0,0 +1,25 @@
+﻿namespace ParallelMatrixMultiplication
+{
+    /// <summary>
+    /// Class with matrix multiplication
+    /// </summary>
+    public class MatrixFunctions
+    {
+        public static int[,] MatrixMuptiplication(int[,] matrix1, int[,] matrix2)
+        {
+            var matrix = new int[matrix1.Rank, matrix1.GetLength(0)];
+            for (int i = 0; i < matrix1.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix1.GetLength(i); j++)
+                {
+                    for (int k = 0; k < matrix1.GetLength((0)); ++k)
+                    {
+                        matrix[i, j] += matrix1[i, k] * matrix2[k, j];   
+                    }
+                }
+            }
+
+            return matrix;
+        }
+    }
+}
