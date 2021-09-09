9b217a42408f76959f78d2a685975465ebf4b7f5
MikePuzanov <mikepuzanov@mail.ru>
2021-09-09 22:20:28 +0300

    delete and add files

---
+++ ParallelMatrixMultiplication/ParallelMatrixMultiplication/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+
+namespace ParallelMatrixMultiplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var threads = new Thread[2];
+            var chunkSize = args.Length / threads.Length;
+            var results = new int[threads.Length];
+            int[,] matrixFirst;
+            int[,] matrixSecond;
+            threads[0] = new Thread(() =>
+            {
+                matrixFirst = FileFunctions.CreateMatrix((args[0]));
+            });
+            threads[1] = new Thread(() =>
+            {
+                matrixSecond = FileFunctions.CreateMatrix((args[1]));
+            });
+            var matrix = MatrixFunctions.MatrixMuptiplication(matrixFirst, matrixSecond);
+        }
+    }
+}
