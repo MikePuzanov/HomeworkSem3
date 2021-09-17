54346a5b2af4f80683de9934a45f32ed7cb3f586
MikePuzanov <mikepuzanov@mail.ru>
2021-09-17 14:35:48 +0300

    fix

--- ParallelMatrixMultiplication/ParallelMatrixMultiplication.Test/MatrixFunctionTest.cs
+++ ParallelMatrixMultiplication/ParallelMatrixMultiplication.Test/MatrixFunctionTest.cs
@@ -32,29 +32,5 @@ namespace ParallelMatrixMultiplication.Test
             var matrix2 = new int[3,3];
             Assert.Throws<MultiplicationException>(() => MatrixFunctions.MatrixMultiplicationParallel(matrix1, matrix2));
         }
-        
-        [Test]
-        public void TestIsParallelFaster()
-        {
-            var matrixTest = new int[1000,1000];
-            Stopwatch stopWatch = new Stopwatch();
-            stopWatch.Start();
-            for (int i = 0; i < 5; i++)
-            {
-                var testTime =MatrixFunctions.MatrixMultiplicationParallel(matrixTest, matrixTest);
-            }
-            stopWatch.Stop();
-            var timeParallel = stopWatch.ElapsedMilliseconds / 5;
-            Stopwatch stopWatchNormal = new Stopwatch();
-            stopWatchNormal.Restart();
-            for (int i = 0; i < 5; i++)
-            {
-                var matrixAnother = MatrixFunctions.MatrixMultiplication(matrixTest, matrixTest);
-            }
-            stopWatchNormal.Stop();
-            var time = stopWatchNormal.ElapsedMilliseconds / 5;
-            Assert.IsTrue(timeParallel < time);
-        }
-
     }
 }
