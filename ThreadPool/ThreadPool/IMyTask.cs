81b20c7b5bb72ebc738d307943a8f58f170a2707
MikePuzanov <mikepuzanov@mail.ru>
2021-10-10 16:57:10 +0300

    base files

---
+++ ThreadPool/ThreadPool/IMyTask.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ThreadPool
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <typeparam name="TResult"></typeparam>
+    public interface IMyTask<TResult>
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public bool IsCompleted { get; set; }
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        public TResult Result { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="func"></param>
+        /// <typeparam name="TNewResult"></typeparam>
+        /// <returns></returns>
+        public IMyTask<TNewResult> ContinueWith<TNewResult>(Func<TResult, TNewResult> func);
+    }
+}
---
+++ ThreadPool/ThreadPool/MyThreadPool.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading;
+
+namespace ThreadPool
+{
+    public class MyThreadPool
+    {
+        private Thread[] _threads;
+        
+        MyThreadPool(int countThreads)
+        {
+            if (countThreads < 1)
+            {
+                throw new ArgumentException();
+            }
+
+            _threads = new Thread[countThreads];
+        }
+
+        public void Shutdown()
+        {
+            
+        }
+    }
+}
