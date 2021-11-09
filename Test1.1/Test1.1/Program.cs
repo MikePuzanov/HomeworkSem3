dc5651e00bb5a913227cc7a1e6883f5fbe2358cb
MikePuzanov <mikepuzanov@mail.ru>
2021-11-09 17:30:06 +0300

    test

---
+++ Test1.1/Test1.1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Test1._1
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            if (args.Length == 1)
+            {
+                var server = new Server(Int32.Parse(args[0]));
+                await server.Working();
+            }
+            else
+            {
+                var client = new Client(args[0], Int32.Parse(args[1]));
+                client.Working();
+            }
+        }
+    }
+}
