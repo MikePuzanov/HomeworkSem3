1fe5b980d0a07c39e4d2bb752613fc205a8e6b70
MikePuzanov <mikepuzanov@mail.ru>
2021-12-06 16:19:21 +0300

    fix test

--- Lazyy/Lazyy.Test/SingleThreadTest.cs
+++ Lazyy/Lazyy.Test/SingleThreadTest.cs
@@ -1,27 +1,69 @@
 using System;
+using System.Collections.Generic;
+using System.Threading;
 using NUnit.Framework;
 
 namespace Lazyy.Test
 {
     public class SingleThreadTest
     {
-        [Test]
-        public void NormalWorkForSingleTest()
+        private static IEnumerable<TestCaseData> Lazy()
         {
-            var number = 2;
-            var lazySingle = LazyFactory.CreateSingleLazy<int>(() =>
-            {
-                number += number;
-                return number;
-            });
+            int countForSingle = 0;
+            int countForMulti = 0;
+            yield return new TestCaseData(LazyFactory.CreateSingleLazy(() => ++countForSingle));
+            yield return new TestCaseData(LazyFactory.CreateMultiLazy(() => Interlocked.Increment(ref countForMulti)));
+        }
+        
+        [Test]
+        public void SupplierCannotBeNull()
+        {
+            Assert.Throws<ArgumentNullException>(() => LazyFactory.CreateSingleLazy<int>(null));
+            Assert.Throws<ArgumentNullException>(() => LazyFactory.CreateMultiLazy<int>(null));
+        }        
+        
+        [TestCaseSource(nameof(Lazy))]
+        public void TestLazyLaunchFunctionOnly1Time<T>(ILazy<T> lazy)
+        {
             for (int i = 0; i < 100; i++)
             {
-                Assert.AreEqual(4, lazySingle.Get());
+                Assert.AreEqual(1, lazy.Get());
+            }
+        }
+        
+        [TestCaseSource(nameof(Lazy))]
+        public void GetShouldNotChangeValue<T>(ILazy<T> lazy)
+        {
+            var value = lazy.Get();
+            Assert.AreEqual(1, value);
+            for (int i = 0; i < 25; i++)
+            {
+                Assert.AreEqual(value, lazy.Get());
             }
         }
 
         [Test]
-        public void NullExceptionTest()
-            => Assert.Throws<ArgumentNullException>(() => LazyFactory.CreateSingleLazy<int>(null));
+        public void RaceConditionsCheck()
+        {
+            var count = 0;
+            var lazy = LazyFactory.CreateMultiLazy(() => Interlocked.Increment(ref count));
+            var threads = new Thread[10];
+
+            for (int i = 0; i < threads.Length; ++i)
+            {
+                threads[i] = new Thread(() => lazy.Get());
+            }
+
+            foreach (var thread in threads)
+            {
+                thread.Start();
+            }
+            foreach (var thread in threads)
+            {
+                thread.Join();
+            }
+
+            Assert.AreEqual(1, count);
+        }
     }
 }
