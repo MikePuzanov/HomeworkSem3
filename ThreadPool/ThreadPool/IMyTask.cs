a692a03911643701cabe7e995ae4879b02fde3d3
MikePuzanov <mikepuzanov@mail.ru>
2022-03-09 13:45:45 +0300

    fix

--- ThreadPool/ThreadPool/IMyTask.cs
+++ ThreadPool/ThreadPool/IMyTask.cs
@@ -1,25 +1,22 @@
 ﻿using System;
 
-namespace ThreadPool
+/// <summary>
+/// interface for object from MyThreadPool
+/// </summary>
+public interface IMyTask<out TResult>
 {
     /// <summary>
-    /// interface for object from MyThreadPool
+    /// shows whether the task is completed or not
     /// </summary>
-    public interface IMyTask<out TResult>
-    {
-        /// <summary>
-        /// shows whether the task is completed or not
-        /// </summary>
-        public bool IsCompleted { get; set; }
+    public bool IsCompleted { get; set; }
 
-        /// <summary>
-        /// returns the result of the task
-        /// </summary>
-        public TResult Result { get; }
+    /// <summary>
+    /// returns the result of the task
+    /// </summary>
+    public TResult Result { get; }
 
-        /// <summary>
-        ///  continues calculating
-        /// </summary>
-        public IMyTask<TNewResult> ContinueWith<TNewResult>(Func<TResult, TNewResult> func);
-    }
+    /// <summary>
+    ///  continues calculating
+    /// </summary>
+    public IMyTask<TNewResult> ContinueWith<TNewResult>(Func<TResult, TNewResult> func);
 }
--- ThreadPool/ThreadPool/MyThreadPool.cs
+++ ThreadPool/ThreadPool/MyThreadPool.cs
@@ -5,209 +5,203 @@ using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace ThreadPool
+/// <summary>
+/// pool of threads that can be used to execute tasks
+/// </summary>
+public class MyThreadPool
 {
+    private readonly Thread[] _threads;
+    private readonly CancellationTokenSource _token = new();
+    private readonly ConcurrentQueue<Action> _tasksQueue = new();
+    private readonly AutoResetEvent _waiterNewTask = new AutoResetEvent(false);
+    private readonly AutoResetEvent _waiterTaskDone = new AutoResetEvent(false);
+    private volatile int _freeThreadsCount = 0;
+    private readonly object _lockObject = new();
+
     /// <summary>
-    /// pool of threads that can be used to execute tasks
+    /// constructor
     /// </summary>
-    public class MyThreadPool
+    public MyThreadPool(int countThreads)
     {
-        private readonly Thread[] _threads;
-        private readonly CancellationTokenSource _token = new();
-        private readonly ConcurrentQueue<Action> _tasksQueue = new();
-        private readonly AutoResetEvent _waiterNewTask = new AutoResetEvent(false);
-        private readonly AutoResetEvent _waiterTaskDone = new AutoResetEvent(false);
-        private int _freeThreadsCount = 0;
-        private readonly object _lockObject = new();
-
-        /// <summary>
-        /// constructor
-        /// </summary>
-        public MyThreadPool(int countThreads)
+        if (countThreads < 1)
         {
-            if (countThreads < 1)
-            {
-                throw new ArgumentOutOfRangeException(nameof(countThreads));
-            }
-
-            _threads = new Thread[countThreads];
-            for (int i = 0; i < countThreads; i++)
-            {
-                _threads[i] = new Thread(() =>
-                {
-                    while (!_token.IsCancellationRequested || !_tasksQueue.IsEmpty)
-                    {
-                        if (_tasksQueue.TryDequeue(out Action action))
-                        {
-                            action();
-                        }
-                        else
-                        {
-                            _waiterNewTask.WaitOne();
-                            if (!_tasksQueue.IsEmpty)
-                            {
-                                _waiterNewTask.Set();
-                            }
-                        }
-                    }
-
-                    Interlocked.Increment(ref _freeThreadsCount);
-                    _waiterTaskDone.Set();
-                });
-
-                _threads[i].Start();
-            }
+            throw new ArgumentOutOfRangeException(nameof(countThreads));
         }
 
-        /// <summary>
-        /// add task 
-        /// </summary>
-        public IMyTask<TResult> AddTask<TResult>(Func<TResult> function)
+        _threads = new Thread[countThreads];
+        for (int i = 0; i < countThreads; i++)
+        {
+            _threads[i] = new Thread(() =>
+            {
+                while (!_token.IsCancellationRequested || !_tasksQueue.IsEmpty)
+                {
+                    if (_tasksQueue.TryDequeue(out Action action))
+                    {
+                        action();
+                    }
+                    else
+                    {
+                        _waiterNewTask.WaitOne();
+                        if (!_tasksQueue.IsEmpty)
+                        {
+                            _waiterNewTask.Set();
+                        }
+                    }
+                }
+
+                Interlocked.Increment(ref _freeThreadsCount);
+                _waiterTaskDone.Set();
+            });
+
+            _threads[i].Start();
+        }
+    }
+
+    /// <summary>
+    /// add task 
+    /// </summary>
+    public IMyTask<TResult> AddTask<TResult>(Func<TResult> function)
+    {
+        ReadyToAddNewTask();
+        lock (_lockObject)
         {
             ReadyToAddNewTask();
-            lock (_lockObject)
-            {
-                if (_token.IsCancellationRequested)
-                {
-                    throw new InvalidOperationException();
-                }
-                var task = new MyTask<TResult>(function, this);
-                AddInTaskQueue(task.Count);
-                return task;
-            }
+            var task = new MyTask<TResult>(function, this);
+            AddInTaskQueue(task.Count);
+            return task;
+        }
+    }
+
+    private void ReadyToAddNewTask()
+    {
+        if (_token.IsCancellationRequested)
+        {
+            throw new InvalidOperationException();
+        }
+    }
+
+    private void AddInTaskQueue(Action task)
+    {
+        _tasksQueue.Enqueue(task);
+        _waiterNewTask.Set();
+    }
+
+    /// <summary>
+    /// close threads
+    /// </summary>
+    public void Shutdown()
+    {
+        lock (_lockObject)
+        {
+            _token.Cancel();
         }
 
-        private void ReadyToAddNewTask()
+        while (_freeThreadsCount != _threads.Length)
         {
-            if (_token.IsCancellationRequested)
-            {
-                throw new InvalidOperationException();
-            }
-        }
-
-        private void AddInTaskQueue(Action task)
-        {
-            _tasksQueue.Enqueue(task);
             _waiterNewTask.Set();
+            _waiterTaskDone.WaitOne();
         }
+    }
+
+    /// <summary>
+    /// class for parallel tasks
+    /// </summary>
+    private class MyTask<TResult> : IMyTask<TResult>
+    {
+        private Func<TResult> _func;
+        private TResult _result;
+        private Exception _resultException = null;
+        private readonly object _locker = new();
+        private readonly ManualResetEvent _waiterManual = new(false);
+        private Queue<Action> _continueWithTasksQueue = new();
+        private readonly MyThreadPool _myThreadPool;
 
         /// <summary>
-        /// close threads
+        /// shows whether the task is completed or not
         /// </summary>
-        public void Shutdown()
-        {
-            lock (_lockObject)
-            {
-                _token.Cancel();
-            }
-
-            while (_freeThreadsCount != _threads.Length)
-            {
-                _waiterNewTask.Set();
-                _waiterTaskDone.WaitOne();
-                _waiterTaskDone.Reset();
-            }
-        }
+        public bool IsCompleted { get; set; }
 
         /// <summary>
-        /// class for parallel tasks
+        /// returns the result of the task
         /// </summary>
-        private class MyTask<TResult> : IMyTask<TResult>
+        public TResult Result
         {
-            private Func<TResult> _func;
-            private TResult _result;
-            private Exception _resultException = null;
-            private readonly object _locker = new();
-            private readonly ManualResetEvent _waiterManual = new(false);
-            private Queue<Action> _continueWithTasksQueue = new();
-            private readonly MyThreadPool _myThreadPool;
-
-            /// <summary>
-            /// shows whether the task is completed or not
-            /// </summary>
-            public bool IsCompleted { get; set; }
-
-            /// <summary>
-            /// returns the result of the task
-            /// </summary>
-            public TResult Result
+            get
             {
-                get
+                _waiterManual.WaitOne();
+                if (_resultException != null)
                 {
-                    _waiterManual.WaitOne();
-                    if (_resultException != null)
+                    throw new AggregateException(_resultException);
+                }
+
+                return _result;
+            }
+        }
+
+        /// <summary>
+        ///  constructor
+        /// </summary>
+        public MyTask(Func<TResult> function, MyThreadPool myThreadPool)
+        {
+            ArgumentNullException.ThrowIfNull(function);
+            _func = function;
+            _myThreadPool = myThreadPool;
+        }
+
+        /// <summary>
+        /// counting result of task
+        /// </summary>
+        public void Count()
+        {
+            try
+            {
+                _result = _func();
+            }
+            catch (Exception funcException)
+            {
+                _resultException = funcException;
+            }
+
+            lock (_locker)
+            {
+                _func = null;
+                IsCompleted = true;
+                _waiterManual.Set();
+            }
+
+            lock (_locker)
+            {
+                while (_continueWithTasksQueue.Count > 0)
+                {
+                    var action = _continueWithTasksQueue.Dequeue();
+                    lock (_myThreadPool._lockObject)
                     {
-                        throw new AggregateException(_resultException);
+                        _myThreadPool.AddInTaskQueue(action);
                     }
-
-                    return _result;
                 }
             }
+        }
 
-            /// <summary>
-            ///  constructor
-            /// </summary>
-            public MyTask(Func<TResult> function, MyThreadPool myThreadPool)
+        /// <summary>
+        ///  continues calculating
+        /// </summary>
+        public IMyTask<TNewResult> ContinueWith<TNewResult>(Func<TResult, TNewResult> func)
+        {
+            lock (_myThreadPool._lockObject)
             {
-                _func = function ?? throw new ArgumentNullException(nameof(function));
-                _myThreadPool = myThreadPool;
-            }
-
-            /// <summary>
-            /// counting result of task
-            /// </summary>
-            public void Count()
-            {
-                try
-                {
-                    _result = _func();
-                }
-                catch (Exception funcException)
-                {
-                    _resultException = funcException;
-                }
-
-                lock (_locker)
-                {
-                    _func = null;
-                    IsCompleted = true;
-                    _waiterManual.Set();
-                }
-
-                lock (_locker)
+                _myThreadPool.ReadyToAddNewTask();
+                var task = new MyTask<TNewResult>(() => func(Result), _myThreadPool);
+                _continueWithTasksQueue.Enqueue(task.Count);
+                if (IsCompleted)
                 {
                     while (_continueWithTasksQueue.Count > 0)
                     {
                         var action = _continueWithTasksQueue.Dequeue();
-                        lock (_myThreadPool._lockObject)
-                        {
-                            _myThreadPool.AddInTaskQueue(action);
-                        }
+                        _myThreadPool._tasksQueue.Enqueue(action);
+                        _myThreadPool._waiterNewTask.Set();
                     }
                 }
-            }
-
-            /// <summary>
-            ///  continues calculating
-            /// </summary>
-            public IMyTask<TNewResult> ContinueWith<TNewResult>(Func<TResult, TNewResult> func)
-            {
-                lock (_myThreadPool._lockObject)
-                {
-                    _myThreadPool.ReadyToAddNewTask();
-                    var task = new MyTask<TNewResult>(() => func(Result), _myThreadPool);
-                    _continueWithTasksQueue.Enqueue(task.Count);
-                    if (IsCompleted)
-                    {
-                        while (_continueWithTasksQueue.Count > 0)
-                        {
-                            var action = _continueWithTasksQueue.Dequeue();
-                            _myThreadPool._tasksQueue.Enqueue(action);
-                            _myThreadPool._waiterNewTask.Set();
-                        }
-                    }
-                    return task;
-                }
+                return task;
             }
         }
     }
